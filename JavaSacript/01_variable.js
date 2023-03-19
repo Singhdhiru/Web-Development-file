0f6e52e169cdd5ee3bbcaa7b0cfc5e298f380dc6
Dhiraj singh <dhirajsingh1224233@gmail.com>
2023-03-20 02:07:13 +0530

    create webDev repo

---
+++ JavaSacript/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 1234242
+let accountEmail = "dhirajsingh1224233@gmail.com";
+var accountPassword = "12390";
+// const ko update anhi kar sakte hi b/c error produce
+// we can update value of let ,var
+
+accountEmail="dhirajsingh37392@gmail.com";
+accountPassword="8080800"
+/* we never use var
+becuse issue in the block scope and functional scope
+*/
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword])
---
+++ JavaSacript/02_DataType.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3); // code readability should be high
+
+console.log("Hitesh");
+
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = false;
+let state;
+// these are primitive data type
+// number => 2 to power 53 -> range of number
+// bigint-> jayada ham number hi prefer karte hi
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined -> abhi valuse asign nahi huaa hi
+// symbol => unique
+
+// object Data in next video
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+// typeof->tell which type of data present memory
